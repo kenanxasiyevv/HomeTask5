68d8f3638aace81841d4d3c97628fdfc05235a02
kenanxasiyevv <kananakh@code.edu.az>
2022-12-28 14:40:57 +0400

    Add project files.

---
+++ HomeTask5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace HomeTask5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Eded daxil et: ");
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int r = 1; r < number; r++)
+            {
+                if (number > r * r)
+                    sum++;
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
